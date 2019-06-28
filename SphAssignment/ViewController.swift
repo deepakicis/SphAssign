bf34d40b6c4d8606340feeae0e367cd8f427d69a
Deepak Kumar <deepak.kumar@aia.com>
2019-06-28 11:26:15 +0800

    Initial Commit

---
+++ SphAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SphAssignment
+//
+//  Created by Deepak Kumar on 28/6/19.
+//  Copyright © 2019 Deepak Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
